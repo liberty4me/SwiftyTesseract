3c4a043e7291cf9b252afebc7c2de14ca9f13aa9
Steven Sherry <steven.r.sherry@gmail.com>
2019-05-13 21:12:52 -0500

    Replace `TesseractVariableName` with `TesseractOption` enum to handle Tesseract variables. This eliminates the need to add a property to `SwiftyTesseract` any time an addition to support a Tesseract addition is needed/requested.
    Conform TesseractOption to Hashable and provide a custom implementation. This prevents allowing multiple variables of the
    same name occupying a Set, even though they may have different values contained in them.
    
    Create `InternalTesseractVariable` enum. This is to handle cases where a somewhat estoeric Tesseract variable
    is needed to be set to make another one work. The one and only current case for this is `oldCharacterHeight`.
    In SwiftyTesseract 2.x, for `minimumCharacterHeight` to work after setting a new value, `oldCharacterHeight` had to
    be set to 1. This was hidden from the user and done in the didSet implementation of `minimumCharacterHeight` when
    it was a property on SwiftyTesseract. Since the properties have been replaced in favor of the TesseractOption enum,
    it posed an issue exposing `oldCharacterHeight` as a case because it feels clunky, confusing, and would be a burden on
    the user of the library.
    
    Create TesseractEnvironment enum. This really is soley for the purpose of setting the TESSDATA_PREFIX environment variable
    for Tesseract to access. It didnt feel right to include it alongside oldCharacterHeight, and I wanted to avoid the ability
    of being able to set oldCharacterHeight as an environment variable.

---
+++ SwiftyTesseract/SwiftyTesseract/Enums/TesseractOption.swift
@@ -0,0 +1,128 @@
+//
+//  TesseractVariableName.swift
+//  SwiftyTesseract
+//
+//  Created by Steven Sherry on 3/24/18.
+//  Copyright © 2018 Steven Sherry. All rights reserved.
+//
+import libtesseract
+
+public enum TesseractOption {
+  /// **Only available for** `EngineMode.tesseractOnly`.
+  /// **Setting** `whiteList` **in any other EngineMode will do nothing**.
+  ///
+  /// Sets a `String` of characters that will **only** be recognized. This does **not** filter values.
+  ///
+  /// Example: setting a whiteList of "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+  /// with an image containing digits may result in "1" being recognized as "I" and "2" being
+  /// recognized as "Z". Set this value **only** if it is 100% certain the characters that are
+  /// defined will **only** be present during recognition.
+  ///
+  /// **This may cause unpredictable recognition results if characters not defined in whiteList**
+  /// **are present**. If **removal** and not **replacement** is desired, filtering the recognition
+  /// string is a better option.
+  case whiteList(String)
+  /// **Only available for** `EngineMode.tesseractOnly`.
+  /// **Setting** `blackList` **in any other EngineMode will do nothing**.
+  ///
+  /// Sets a `String` of characters that will **not** be recognized. This does **not** filter values.
+  ///
+  /// Example: setting a blackList of "0123456789" with an image containing digits may result in
+  /// "1" being recognized as "I" and "2" being recognized as "Z". Set this value **only** if it
+  /// is 100% certain that the characters defined will **not** be present during recognition.
+  ///
+  /// **This may cause unpredictable recognition results if characters defined in blackList are**
+  /// **present**. If **removal** and not **replacement** is desired, filtering the recognition
+  /// string is a better option
+  case blackList(String)
+  /// Preserve multiple interword spaces
+  case preserveInterwordSpaces(Bool)
+  /// Minimum character height
+  case minimumCharacterHeight(Int)
+  
+  var value: String {
+    switch self {
+    case .whiteList(let string): return string
+    case .blackList(let string): return string
+    case .preserveInterwordSpaces(let bool): return bool ? "1" : "0"
+    case .minimumCharacterHeight(let int): return "\(int)"
+    }
+  }
+  
+  func setVariable(to tesseract: TessBaseAPI) {
+    switch self {
+    case .minimumCharacterHeight:
+      let oldHeight = InternalTesseractVariable.oldCharacterHeight(1)
+      TessBaseAPISetVariable(tesseract, oldHeight.description, oldHeight.value)
+      TessBaseAPISetVariable(tesseract, description, value)
+    default:
+      TessBaseAPISetVariable(tesseract, description, value)
+    }
+    
+  }
+}
+
+extension TesseractOption: CustomStringConvertible {
+  public var description: String {
+    switch self {
+    case .whiteList: return "tessedit_char_whitelist"
+    case .blackList: return "tessedit_char_blacklist"
+    case .preserveInterwordSpaces: return "preserve_interword_spaces"
+    case .minimumCharacterHeight: return "textord_min_xheight"
+    }
+  }
+}
+
+extension TesseractOption: Hashable {
+  public static func ==(lhs: TesseractOption, rhs: TesseractOption) -> Bool {
+    return lhs.description == rhs.description
+  }
+  
+  public func hash(into hasher: inout Hasher) {
+    hasher.combine(description)
+  }
+}
+
+extension Set where Element == TesseractOption {
+  mutating func insert(option: TesseractOption) {
+    if contains(option) {
+      remove(option)
+    }
+    insert(option)
+  }
+}
+
+enum InternalTesseractVariable: CustomStringConvertible {
+  case oldCharacterHeight(Int)
+  
+  var description: String {
+    switch self {
+    case .oldCharacterHeight: return "textord_old_xheight"
+    }
+  }
+  
+  var value: String {
+    switch self {
+    case .oldCharacterHeight(let int): return "\(int)"
+    }
+  }
+}
+
+enum TesseractEnvironment {
+  case tessDataPrefix(String)
+  
+  var value: String {
+    switch self {
+    case .tessDataPrefix(let prefix): return prefix
+    }
+  }
+}
+
+extension TesseractEnvironment: CustomStringConvertible {
+  var description: String {
+    switch self {
+    case .tessDataPrefix: return "TESSDATA_PREFIX"
+    }
+  }
+}
+
